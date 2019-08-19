f22c25735a91cd4f4fafa37a6b92c3a15af5bbf6
dasilvar93 <kadasilva22@gmail.com>
2019-08-19 09:14:38 -0700

    fixed bug with merge method, and wrote 3 more for remaining directions. Wrote tests for all of them, all working

--- src/board.rs
+++ src/board.rs
@@ -12,7 +12,7 @@ struct Board{
 
 impl Board {
 
-    fn init_grid() -> Self{
+    pub fn init_grid() -> Self{
         let mut row: Vec<Vec<Tile>> = Vec::new();
         for x in 0..4 {
             let mut col: Vec<Tile> = Vec::new();
@@ -121,19 +121,79 @@ impl Board {
         }    
     }
 
-    fn merge_left(&mut self) {
+    pub fn merge_left(&mut self) {
         self.mov_left();
         let grid = &mut self.grid;
         for y in 0 .. 4 {
             for x in 0 .. 3 {
                 let curr = &mut grid[x][y].clone();
                 let next = &mut grid[x+1][y].clone();
-                curr.merge(next);
+                if curr.get_val() == next.get_val() && curr.get_val() != None {
+                    curr.merge(next);
+                    grid[x][y] = curr.clone();
+                    grid[x+1][y] = next.clone();
+                }
+                
             }
         }
         self.mov_left();
-
     }
+
+    pub fn merge_right(&mut self){
+        self.mov_right();
+        let grid = &mut self.grid;
+        for y in 0 .. 4 {
+            for x in (1 .. 4).rev() {
+                let curr = &mut grid[x][y].clone();
+                let next = &mut grid[x-1][y].clone();
+                if curr.get_val() == next.get_val() && curr.get_val() != None {
+                    curr.merge(next);
+                    grid[x][y] = curr.clone();
+                    grid[x-1][y] = next.clone();
+                }
+                
+            }
+        }
+        self.mov_right();
+    }
+
+    pub fn merge_up(&mut self){
+        self.mov_up();
+        let grid = &mut self.grid;
+        for x in 0 .. 4 {
+            for y in 0 .. 3 {
+                let curr = &mut grid[x][y].clone();
+                let next = &mut grid[x][y+1].clone();
+                if curr.get_val() == next.get_val() && curr.get_val() != None {
+                    curr.merge(next);
+                    grid[x][y] = curr.clone();
+                    grid[x][y+1] = next.clone();
+                }
+                
+            }
+        }
+        self.mov_up();
+    }
+
+    pub fn merge_down(&mut self){
+        self.mov_down();
+        let grid = &mut self.grid;
+        for x in 0 .. 4 {
+            for y in (1 .. 4).rev() {
+                let curr = &mut grid[x][y].clone();
+                let next = &mut grid[x][y-1].clone();
+                if curr.get_val() == next.get_val() && curr.get_val() != None {
+                    curr.merge(next);
+                    grid[x][y] = curr.clone();
+                    grid[x][y-1] = next.clone();
+                }
+                
+            }
+        }
+        self.mov_down();
+    }
+
+
 
 }
 #[cfg(test)]
@@ -252,5 +312,91 @@ mod board_tests {
         assert_eq!(None, test_board.grid[1][0].get_val());
         assert_eq!(None, test_board.grid[2][0].get_val());
         assert_eq!(None, test_board.grid[3][0].get_val());
+    }
+
+    #[test]
+    fn test_merge_left_multiple_same(){
+        let mut test_board = Board::init_grid();
+        test_board.grid[3][0].set_val(2);
+        test_board.grid[2][0].set_val(2);
+        test_board.grid[1][0].set_val(2);
+        test_board.grid[0][0].set_val(2);
+        test_board.merge_left();
+        assert_eq!(4, test_board.grid[0][0].get_val().unwrap());
+        assert_eq!(4, test_board.grid[1][0].get_val().unwrap());
+        assert_eq!(None, test_board.grid[2][0].get_val());
+        assert_eq!(None, test_board.grid[3][0].get_val());
+    }
+
+    #[test]
+    fn test_merge_left_multiple_rows_mixed_values(){
+        let mut test_board = Board::init_grid();
+        test_board.grid[3][0].set_val(2);
+        test_board.grid[2][0].set_val(2);
+        test_board.grid[1][0].set_val(8);
+        test_board.grid[0][0].set_val(2);
+        test_board.grid[2][3].set_val(4);
+        test_board.grid[1][3].set_val(4);
+        test_board.merge_left();
+        assert_eq!(2, test_board.grid[0][0].get_val().unwrap());
+        assert_eq!(8, test_board.grid[1][0].get_val().unwrap());
+        assert_eq!(4, test_board.grid[2][0].get_val().unwrap());
+        assert_eq!(None, test_board.grid[3][0].get_val());
+        assert_eq!(8, test_board.grid[0][3].get_val().unwrap());
+        assert_eq!(None, test_board.grid[1][3].get_val());
+    }
+
+    #[test]
+    fn test_merge_right_simple() {
+        let mut test_board = Board::init_grid();
+        test_board.grid[0][0].set_val(2);
+        test_board.grid[1][0].set_val(2);
+        test_board.merge_right();
+        assert_eq!(4, test_board.grid[3][0].get_val().unwrap());
+        assert_eq!(None, test_board.grid[1][0].get_val());
+        assert_eq!(None, test_board.grid[2][0].get_val());
+        assert_eq!(None, test_board.grid[0][0].get_val());
+    }
+
+    #[test]
+    fn test_merge_right_multiple_rows_mixed_values() {
+        let mut test_board = Board::init_grid();
+        test_board.grid[3][0].set_val(2);
+        test_board.grid[2][0].set_val(2);
+        test_board.grid[1][0].set_val(8);
+        test_board.grid[0][0].set_val(2);
+        test_board.grid[2][3].set_val(4);
+        test_board.grid[1][3].set_val(4);
+        test_board.merge_right();
+        assert_eq!(None, test_board.grid[0][0].get_val());
+        assert_eq!(2, test_board.grid[1][0].get_val().unwrap());
+        assert_eq!(8, test_board.grid[2][0].get_val().unwrap());
+        assert_eq!(4, test_board.grid[3][0].get_val().unwrap());
+        assert_eq!(8, test_board.grid[3][3].get_val().unwrap());
+        assert_eq!(None, test_board.grid[1][3].get_val());
+    }
+
+    #[test]
+    fn test_merge_up_simple() {
+        let mut test_board = Board::init_grid();
+        test_board.grid[0][0].set_val(2);
+        test_board.grid[0][2].set_val(2);
+        test_board.merge_up();
+        assert_eq!(4, test_board.grid[0][0].get_val().unwrap());
+        assert_eq!(None, test_board.grid[0][1].get_val());
+        assert_eq!(None, test_board.grid[0][2].get_val());
+        assert_eq!(None, test_board.grid[0][3].get_val());
+    }
+
+    #[test]
+    fn test_merge_down_simple(){
+        let mut test_board = Board::init_grid();
+        test_board.grid[0][0].set_val(2);
+        test_board.grid[0][2].set_val(2);
+        test_board.merge_down();
+        assert_eq!(4, test_board.grid[0][3].get_val().unwrap());
+        assert_eq!(None, test_board.grid[0][1].get_val());
+        assert_eq!(None, test_board.grid[0][2].get_val());
+        assert_eq!(None, test_board.grid[0][0].get_val());
     }
 }
